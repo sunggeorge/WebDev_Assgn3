32eead36b1bdbd0adca3d583b43895799e6afae2
sunggeorge <sunggeorge@gmail.com>
2023-12-15 03:31:15 -0700

    Bug fix for js 2

--- scripts/contact.js
+++ scripts/contact.js
@@ -2,10 +2,12 @@
 
 // hint: you can change the style of an element by modifying the value of that element's .style.fontSize, or by updating its .classList.
 
+/*jshint esversion: 6 */
+
 const contactPage = document.getElementById('contact-page');
 const submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', () => {
-    contactPage.innerHTML = "<p>Thank you for your message!</p>"
-    contactPage.classList.add('submitted')
-})
+    contactPage.innerHTML = "<p>Thank you for your message!</p>";
+    contactPage.classList.add('submitted');
+});
